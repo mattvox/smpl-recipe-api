aeef41be9a30ded9011e8c078ff91c694938d885
mattvox <mattbliffert@icloud.com>
2017-10-11 21:16:51 -0400

    Updated queries to be case insensitive.

--- queries/queries.js
+++ queries/queries.js
@@ -1,8 +1,6 @@
 const promise = require('bluebird')
 
-const options = {
-  promiseLib: promise,
-}
+const options = { promiseLib: promise }
 
 const pgp = require('pg-promise')(options)
 
@@ -16,32 +14,12 @@ const connectionString = {
 
 const db = pgp(connectionString)
 
-// query functions
-
-function getAllRecipes(req, res, next) {
-  const offset = 0
-  const limit = 12
-
-  db.any(`select * from recipes_recipe limit ${limit} offset ${offset}`)
-  // db.any('select * from recipes_recipe')
-    .then((data) => {
-      res.status(200)
-        // .json({
-        //   status: 'success',
-        //   data: data,
-        //   message: 'Retrieved ALL recipes',
-        // });
-        .json(data)
-    })
-    .catch(err => next(err))
-}
-
 function getRecipes(req, res, next) {
   const offset = req.query.offset || 0
   const limit = 12
 
   if (req.query.search) {
-    db.any(`select * from recipes_recipe where title like '%${req.query.search}%' limit ${limit} offset ${offset}`)
+    db.any(`select * from recipes_recipe where lower(title) like '%${req.query.search}%' limit ${limit} offset ${offset}`)
       .then((data) => {
         res.status(200)
           .json(data)
@@ -57,13 +35,6 @@ function getRecipes(req, res, next) {
   }
 }
 
-function searchRecipes(req, res, next) {
-  db.any(`select * from recipes_recipe where title like '%${req.query.search}%'`)
-    .then((data) => {
-      res.status(200).json(data)
-    })
-    .catch(err => next(err))
-}
 
 function getRecipe(req, res, next) {
   db.one(`select * from recipes_recipe where id = ${req.params.id}`)
@@ -79,8 +50,36 @@ function getRecipe(req, res, next) {
 }
 
 module.exports = {
-  getAllRecipes,
   getRecipes,
   getRecipe,
-  searchRecipes,
+  // getAllRecipes,
+  // searchRecipes,
 }
+
+// FOR TESTING, REMOVE LATER
+
+// function getAllRecipes(req, res, next) {
+//   const offset = 0
+//   const limit = 12
+//
+//   db.any(`select * from recipes_recipe limit ${limit} offset ${offset}`)
+//   // db.any('select * from recipes_recipe')
+//     .then((data) => {
+//       res.status(200)
+//         // .json({
+//         //   status: 'success',
+//         //   data: data,
+//         //   message: 'Retrieved ALL recipes',
+//         // });
+//         .json(data)
+//     })
+//     .catch(err => next(err))
+// }
+//
+// function searchRecipes(req, res, next) {
+//   db.any(`select * from recipes_recipe where title like '%${req.query.search}%'`)
+//     .then((data) => {
+//       res.status(200).json(data)
+//     })
+//     .catch(err => next(err))
+// }
